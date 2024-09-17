9d90ac065dd3aa9dbd43b8cb92d4bbf9b1020916
Hanna Grehn <hannagrehn@gmail.com>
2024-09-17 13:39:42 +0200

    surface

---
+++ Umbrella/Models/ContactFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Umbrella.Models
+{
+	public class ContactFormModel
+	{
+		public string Name { get; set; } = null!;
+		public string Email { get; set; } = null!;
+		public string Message { get; set; } = null!;
+	}
+}
